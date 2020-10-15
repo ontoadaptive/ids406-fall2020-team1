224ca361fe717bcaceaff6d240c9b395e9dfa285
Joseph <jcoope88@gmail.com>
2020-10-15 16:13:00 -0500

    JC, Models created relationship

--- med-web-app/mysite/main/admin.py
+++ med-web-app/mysite/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Patient, Medication
 # Register your models here.
+admin.site.register(Patient)
+admin.site.register(Medication)
--- med-web-app/mysite/main/models.py
+++ med-web-app/mysite/main/models.py
@@ -8,34 +8,34 @@ class Patient(models.Model):
     GENDER_CHOICES = (
         (MALE, 'M'), 
         (FEMALE, 'F'),
-    )
+        )
 
     patient_id = models.AutoField(primary_key = True)   
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    phone_number = models.CharField(max_length=5)
+    first_name = models.CharField(max_length=20, null = True)
+    last_name = models.CharField(max_length=20, null = True)
+    phone_number = models.CharField(max_length=10, null = True) #773 444 9999
 
     gender = models.CharField(
         max_length=1,
+        null = True,
         choices = GENDER_CHOICES
-    )
-    birth_date = models.DateField()
+        )
+
+    birth_date = models.DateField(auto_now_add = True, null = True)
 
 class Medication(models.Model):
-    POWDER = 'P'
-    TABLETS = 'T'
-    CAPSULES = 'C'
-
-    FORM_CHOICES  = ((POWDER, 'P'), (TABLETS, 'T'), (CAPSULES, 'C'))
+    FORM_CHOICES = (
+        ('Form', 'Form' ),
+        ('Tablet', 'Tablet'),
+        ('Powder', 'Powder'),
+        ('Capsule', 'Capsule')
+        )
 
     identifier =  models.AutoField(primary_key = True) #should we do it this way 
-    expiration_date =  models.DateField()
-    #code =
-    form =  models.CharField(
-        max_length = 1,
-        choices = FORM_CHOICES
-    )
-    amount = models.CharField(max_length = 3) # how many of form do we have?
+    name = models.CharField(max_length = 200, null = True )
+    expiration_date =  models.DateField(auto_now_add = True, null = True)
+    form =  models.CharField(max_length = 10, null = True, choices=FORM_CHOICES)
+    amount = models.CharField(max_length = 3, null = True) # how many of form do we have?
     
-    patient = models.ForeignKey(Patient, on_delete = models.CASCADE ) #what is models.CASCADE
+    patient = models.ManyToManyField(Patient)
     
