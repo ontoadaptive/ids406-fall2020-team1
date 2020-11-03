036456d1b4d3b753e52db51e67f2ef5184915b6f
Ryan Fitch <ryanfitch19@gmail.com>
2020-11-02 23:35:43 -0600

    Create new patient form component

---
+++ bp-web-app/src/components/NewObservationForm.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const NewPatientForm = () => {
+    return (
+  // TODO: Complete this during module #3, November 2020
+  <div></div>
+  );
+};
+
+export default NewPatientForm;
