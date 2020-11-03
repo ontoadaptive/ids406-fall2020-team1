ffb2db192e91041130baab57b555e1dc0fc4f0c3
Ryan Fitch <ryanfitch19@gmail.com>
2020-11-02 23:36:06 -0600

    Add AppHeader and NewObservationForm to the exports

--- bp-web-app/src/components/index.js
+++ bp-web-app/src/components/index.js
@@ -1,3 +1,5 @@
+export { default as AppHeader } from "./AppHeader";
 export { default as MedicationsViewer } from "./MedicationsViewer";
+export { default as NewObservationForm } from "./NewObservationForm";
 export { default as ObservationsViewer } from "./ObservationsViewer";
 export { default as PatientsViewer } from "./PatientsViewer";
